004358dff48f3804f123bd06305807871255ca53
pattyouwehand <pattyouwehand@gmail.com>
2019-09-13 12:02:07 +0200

    setup client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global EventsServer */
+import React from 'react';                    //Socket.io
+//import request from 'superagent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { text: ''}
+
+  source = new EventSource('http://localhost:4000/stream')        //eventsource is buildin 
+
+  componentDidMount(){
+   this.source.onmessage = (event) => {
+     const { data } = event
+     console.log('data test:', data)
+
+     const messages = JSON.parse(data)                            // puts the string back in js
+     console.log('messages test:', messages)
+   }
+  }
+
+  onChange = (event) => {
+    const { target: { value }} = event
+    console.log('value test:', value)
+
+    this.setState({ text: value})
+  }
+
+  render(){
+    return <form onSubmit>
+    <input
+        type='text'
+        onChange={this.onChange}
+        value={this.state.text}
+    />
+    <button
+    onClick={onClick}
+    type="button"
+    >Click</button>
+    </form>
+  }
 }
 
 export default App;
